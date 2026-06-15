81fe752367fa8bea9fa3188c9ba028167478bf78
MGFRV <madghost67@gmail.com>
2026-06-15 12:37:36 +0300

    Allow Vercel builds to skip ESLint

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
