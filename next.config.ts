cc09684e08c752071e0948b1018b4abfb97ac9fb
MGFRV <madghost67@gmail.com>
2026-07-23 09:10:22 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
   eslint: {
     ignoreDuringBuilds: true,
